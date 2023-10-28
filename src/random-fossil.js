432e3f04c02f2996101913cd0a13cb1ee44d6056
Robert Smith <r.reed.smith@gmail.com>
2023-10-27 23:39:02 -0600

    finished!

--- src/random-fossil.js
+++ src/random-fossil.js
@@ -1,1 +1,14 @@
-console.log('Hello World');
+import axios from "axios";
+
+document.querySelector('#get-random-fossil').addEventListener('click',(e) => {
+
+axios.get('/random-fossil.json')
+    .then(res => {
+        const {img, name} = res.data
+        document.querySelector('#random-fossil-image').innerHTML = `<img src=${img}></img>`
+        document.querySelector('#random-fossil-name').innerHTML = `<p>${name}</p>`
+
+    })
+    
+
+})
